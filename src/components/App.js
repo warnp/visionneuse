d0247d727a7e47cf1aac3cbca2da6318ef56838e
Romain <romain.assie@supinfo.com>
2016-07-11 22:55:15 +0200

    core(finishing) : FInish replacing

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Master from './Master';
+
+const App = () => {
+  return (
+    <div>
+      <Master />
+    </div>
+  );
+}
+
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,29 +1,15 @@
-/**
- * App entry point
- */
-
 // Polyfill
 import 'babel-polyfill';
 
-// Libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+import App from './components/App';
 
-// Routes
-import Routes from './common/components/Routes';
+window.React = React;
 
-// Base styling
-import './common/base.css';
-
-
-// ID of the DOM element to mount app on
-const DOM_APP_EL_ID = 'app';
-
-// Render the router
-ReactDOM.render((
-  <Router history={browserHistory}>
-    {Routes}
-  </Router>
-), document.getElementById(DOM_APP_EL_ID));
-
+render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById('app')
+);
