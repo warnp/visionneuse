6f7a3ce4139c3bc94047e1b204d6e58b80045979
Romain <romain.assie@supinfo.com>
2016-07-21 08:15:30 +0200

    feat(router) : Add react-router

--- src/main.js
+++ src/main.js
@@ -3,14 +3,15 @@ import 'babel-polyfill';
 
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App';
 import './styles/style.css';
 
 window.React = React;
 
 render(
-  <div>
-    <App />
-  </div>,
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>,
   document.getElementById('app')
 );
