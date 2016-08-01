7cc33d893d267665acb40231248a351c7b2c881b
romain <romain@debian>
2016-08-01 02:23:10 +0200

    core(data) : Add some data

--- src/data.js
+++ src/data.js
@@ -109,6 +109,387 @@ const data = [{
 }, {
   src: require('./images/20160503/JPEG/IMG_3992.jpg'),
   jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3993.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3994.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3995.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3996.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3997.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3998.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3999.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4000.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4001.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4002.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4003.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4004.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4005.jpg'),
+  jour: 1
+},  {
+  src: require('./images/20160503/JPEG/IMG_4006.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4007.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4008.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4009.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4010.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4011.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4012.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4013.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4014.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4015.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4016.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4017.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4018.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4019.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4020.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_4021.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3973.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3974.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3973.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3978.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3979.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3981.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3982.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3985.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3987.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3990.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3992.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+},{
+  src: require('./images/20160503/JPEG/IMG_3943.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3944.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3945.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3946.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3947.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3948.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3950.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3951.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3953.jpg'),
+  jour: 1
+},  {
+  src: require('./images/20160503/JPEG/IMG_3955.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3957.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3958.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3959.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3960.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3961.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3962.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3963.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3964.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3965.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3966.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3967.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3968.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3969.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3970.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3972.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3973.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3974.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3973.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3978.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3979.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3981.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3982.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3985.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3987.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3990.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3992.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+},{
+  src: require('./images/20160503/JPEG/IMG_3943.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3944.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3945.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3946.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3947.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3948.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3950.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3951.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3953.jpg'),
+  jour: 1
+},  {
+  src: require('./images/20160503/JPEG/IMG_3955.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3957.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3958.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3959.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3960.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3961.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3962.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3963.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3964.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3965.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3966.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3967.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3968.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3969.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3970.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3972.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3973.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3974.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3973.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3978.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3979.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3981.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3982.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3985.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3987.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3990.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3992.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
+}, {
+  src: require('./images/20160503/JPEG/IMG_3991.jpg'),
+  jour: 1
 }];
 
 export default data;
